45a97e267fde8ab9d231a9625e8591ea9c20be85
zhaochengw <zhaochengw@qq.com>
2021-05-30 10:13:29 +0800

    ClassicAutoDismount 飞行点自动下马

---
+++ ClassicAutoDismount/ClassicAutoDismount.lua
@@ -0,0 +1,22 @@
+﻿----------------------------------------------------------------------------------------
+--	ClassicAutoDismount (by EsreverWoW)
+----------------------------------------------------------------------------------------
+local f = CreateFrame("Frame")
+
+local errorMessages = {
+	[ERR_ATTACK_MOUNTED]			= true,
+	[ERR_MOUNT_ALREADYMOUNTED]		= true,
+	[ERR_NOT_WHILE_MOUNTED]			= true,
+	[ERR_TAXIPLAYERALREADYMOUNTED]	= true,
+	[SPELL_FAILED_NOT_MOUNTED]		= true,
+}
+
+local function CheckDismount(self, event, ...)
+	if not IsMounted() or not errorMessages[select(2, ...)] then return end
+
+	Dismount()
+	UIErrorsFrame:Clear()
+end
+
+f:RegisterEvent("UI_ERROR_MESSAGE")
+f:SetScript("OnEvent", CheckDismount)
