9aec55da302b01735cad2064c05ef5eff5b22f02
zhaochengw <zhaochengw@qq.com>
2021-07-03 15:14:02 +0800

    添加FriendsMenuXP 右键增强

---
+++ FriendsMenuXP/localization.zhCN.lua
@@ -0,0 +1,13 @@
+﻿if ( GetLocale() == "zhCN" ) then 
+	FRIENDS_MENU_XP_LOADED = "FriendsMenuXP加载成功"
+	FRIENDS_MENU_XP_CANNOT_TARGET = "FriendsMenuXP无法选定<$name$>为目标。"
+
+	FMXP_BUTTON_ADD_FRIEND = "加为好友"
+	FMXP_BUTTON_SEND_WHO = "== 查询详情"
+	FMXP_BUTTON_ADD_GUILD = "公会邀请"
+	FMXP_BUTTON_GET_NAME = ">> 复制名字"
+	FMXP_BUTTON_ACHIEVEMENTS = "对比成就"
+	FMXP_BUTTON_ARMORY = "英雄榜"
+
+    FMXP_BUTTON_REPORT_PLAYER_FOR = "举报-"
+end
