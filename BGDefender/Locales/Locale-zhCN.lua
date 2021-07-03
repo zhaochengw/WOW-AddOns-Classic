1e6aebc520cdef6d0e93564ceb3bcb07c6ded743
zhaochengw <zhaochengw@qq.com>
2021-07-03 08:34:18 +0800

    添加BGDefender

---
+++ BGDefender/Locales/Locale-zhCN.lua
@@ -0,0 +1,31 @@
+﻿--[[****************************************************************************
+  * Locales/Locale-zhCN.lua - Localized string constants (zh-CN).              *
+  ****************************************************************************]]
+function BGD_init_zhCN()
+    BGD_HELP    = "$base 需要帮忙需要帮忙！！！！"
+    BGD_SAFE    = "$base 已经安全."
+    BGD_INC     = "注意!$base 有 $num 来袭！"
+    BGD_INCPLUS = "救命阿！ $base 对面开火车来！"
+end
+
+if (GetLocale() == "zhCN") then
+    BGD_AV      = "奥特兰克山谷"
+    BGD_AB      = "阿拉希盆地"
+    BGD_WSG     = "战歌峡谷"
+    BGD_WSL     = "战歌伐木场"
+    BGD_SWH     = "银翼要塞"
+    BGD_EOTS    = "暴风之眼"
+    BGD_SOTA    = "远祖滩头"
+    BGD_IOC     = "征服之岛"
+    BGD_TP      = "双子峰"
+    BGD_DMH     = "龙喉要塞"
+    BGD_GIL     = "吉尔尼斯之战"
+
+    BGD_WG      = "冬握湖"
+    BGD_TB      = "托巴拉德"
+
+    BGD_AWAY    = "你并没有靠近任何防卫点"
+    BGD_OUT     = "你不在战场里!"
+
+    BGD_GENERAL = "综合"
+end
---
+++ BGDefender/Locales/Locale-zhTW.lua
@@ -0,0 +1,31 @@
+﻿--[[****************************************************************************
+  * Locales/Locale-zhTW.lua - Localized string constants (zh-TW).              *
+  ****************************************************************************]]
+function BGD_init_zhTW()
+    BGD_HELP    = "$base 需要幫忙需要幫忙！！！！"
+    BGD_SAFE    = "$base 已經安全."
+    BGD_INC     = "注意!$base 有 $num 來襲！"
+    BGD_INCPLUS = "救命阿！ $base 對面開火車來！"
+end
+
+if (GetLocale() == "zhTW") then
+    BGD_AV      = "奧特蘭克山谷"
+    BGD_AB      = "阿拉希盆地"
+    BGD_WSG     = "戰歌峽谷"
+    BGD_WSL     = "戰歌伐木場"
+    BGD_SWH     = "銀翼要塞"
+    BGD_EOTS    = "暴風之眼"
+    BGD_SOTA    = "遠祖灘頭"
+    BGD_IOC     = "征服之島"
+    BGD_TP      = "雙子峰"
+    BGD_DMH     = "龍喉要塞"
+    BGD_GIL     = "吉爾尼斯之戰"
+
+    BGD_WG      = "冬握湖"
+    BGD_TB      = "托巴拉德"
+
+    BGD_AWAY    = "你並沒有靠近任何防衛點"
+    BGD_OUT     = "你不在戰場裡!"
+
+    BGD_GENERAL = "綜合"
+end 
