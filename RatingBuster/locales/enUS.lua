be85e163b54bb5542951da313fd818f7c31605b4
zhaochengw <zhaochengw@qq.com>
2022-05-22 10:09:40 +0800

    RatingBuster属性转换

--- RatingBuster/locales/enUS.lua
+++ RatingBuster/locales/enUS.lua
@@ -485,9 +485,8 @@ L["ItemID: "] = true
 --
 -- Tip2: The strings are passed into string.find, so you should escape the magic characters ^$()%.[]*+-? with a %
 L["numberPatterns"] = {
-	{pattern = " by (%d+)$", addInfo = "AfterNumber",},
-	{pattern = " by (%d+)[^%%]", addInfo = "AfterNumber",},
-	{pattern = "([%+%-]%d+)", addInfo = "AfterStat",},
+	{pattern = " by (%d+)%f[^%d%%]", addInfo = "AfterNumber",},
+	{pattern = "([%+%-]%d+)%f[^%d%%]", addInfo = "AfterStat",},
 	{pattern = "grant.-(%d+)", addInfo = "AfterNumber",}, -- for "grant you xx stat" type pattern, ex: Quel'Serrar, Assassination Armor set
 	{pattern = "add.-(%d+)[^%%]?$", addInfo = "AfterNumber",}, -- for "add xx stat" type pattern, ex: Adamantite Sharpening Stone
 	-- Added [^%%] so that it doesn't match strings like "Increases healing by up to 10% of your total Intellect." [Whitemend Pants] ID: 24261
@@ -530,6 +529,7 @@ SPELL_STAT4_NAME = "Intellect"
 SPELL_STAT5_NAME = "Spirit"
 --]]
 L["statList"] = {
+	{pattern = "lowers intellect of target", id = nil}, -- Brain Hacker
 	{pattern = string.lower(SPELL_STAT1_NAME), id = SPELL_STAT1_NAME}, -- Strength
 	{pattern = string.lower(SPELL_STAT2_NAME), id = SPELL_STAT2_NAME}, -- Agility
 	{pattern = string.lower(SPELL_STAT3_NAME), id = SPELL_STAT3_NAME}, -- Stamina
