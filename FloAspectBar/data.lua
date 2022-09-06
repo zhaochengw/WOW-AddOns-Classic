24191ac76b0fae1e2fbd5d4f9e6f117c27fe25e1
zhaochengw <zhaochengw@qq.com>
2022-09-06 08:15:39 +0800

    FLOASPECT猎人守护助手修复，添加龙鹰守护

--- FloAspectBar/data.lua
+++ FloAspectBar/data.lua
@@ -35,4 +35,17 @@ elseif WOW_PROJECT_ID == WOW_PROJECT_BURNING_CRUSADE_CLASSIC then
 			{ id = 34074 }, -- viper
 		}
 	};
+elseif WOW_PROJECT_ID == WOW_PROJECT_WRATH_CLASSIC then
+	FLO_ASPECT_SPELLS = {
+		["HUNTER"] = {
+			{ id = 13163 }, -- monkey
+			{ id = 13165 }, -- hawk
+			{ id = 5118 }, -- cheetah
+			{ id = 13161 }, -- beast
+			{ id = 13159 }, -- pack
+			{ id = 20043 }, -- nature
+			{ id = 34074 }, -- viper
+			{ id = 61846 }, -- dragonhawk
+		}
+	};
 end
