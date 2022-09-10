b81e582ca4f8fe3df124e0c5edb75de45558ca60
zhaochengw <zhaochengw@qq.com>
2022-09-10 19:48:42 +0800

    Threat Plates 10.5.2

--- TidyPlates_ThreatPlates/Commands.lua
+++ TidyPlates_ThreatPlates/Commands.lua
@@ -5,7 +5,7 @@
 ---------------------------------------------------------------------------------------------------
 local L = Addon.ThreatPlates.L
 
-Addon.DEBUG = Addon.ThreatPlates.Meta("version") == "10.5.1"
+Addon.DEBUG = Addon.ThreatPlates.Meta("version") == "10.5.2"
 
 local function toggleDPS()
 	if Addon.db.profile.optionRoleDetectionAutomatic then
