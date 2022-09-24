2febf89f4adfa3ba0a6b4684a7f19be976b2d725
zhaochengw <zhaochengw@qq.com>
2022-09-24 22:44:54 +0800

    QuestXP 0.8.0

--- QuestXP/QuestXP.lua
+++ QuestXP/QuestXP.lua
@@ -1,6 +1,6 @@
 local addOnName, ns = ...
 local questCache = {}
-local LEVEL_CAP = 70
+local LEVEL_CAP = 80
 
 local QXP = CreateFrame("FRAME")
 QXP:RegisterEvent("ADDON_LOADED")
@@ -63,19 +63,15 @@ function QXP:QuestLog_Update(addonName)
         xpLeveLTag = '**'
     end
 
-    -- pre-calculate zone XP totals (TODO)
-    local numEntries, numQuests = GetNumQuestLogEntries()
-    for i=1, numEntries, 1 do
-        local questLogTitleText, level, questTag, isHeader, isCollapsed, isComplete, frequency, questID, startEvent, displayQuestID, isOnMap, hasLocalPOI, isTask, isBounty, isStory, isHidden, isScaling = GetQuestLogTitle(i);
-        if isHeader then
-            header = questLogTitleText
-            headerXP[header] = 0
-        else
-            if QXPdb[questID] then
-                headerXP[header] = headerXP[header] + QXPdb[questID].XP
-            end
-        end
-    end
+    local numEntries, numQuests = GetNumQuestLogEntries();
+    local scrollOffset = HybridScrollFrame_GetOffset(QuestLogListScrollFrame);
+    local buttons = QuestLogListScrollFrame.buttons;
+    local buttonHeight = buttons[1]:GetHeight();
+    local displayedHeight = 0;
+
+    local questIndex, questLogTitle, questTitleTag, questNumGroupMates, questNormalText, questHighlight, questCheck;
+    local questLogTitleText, level, questTag, isHeader, isCollapsed, isComplete, color;
+    local numPartyMembers, partyMembersOnQuest, tempWidth, textWidth;
 
     if addonName == "QuestLogEx" then
         numQuestsDisplayed = QuestLogEx.db.global.maxQuestsDisplayed
@@ -84,30 +80,37 @@ function QXP:QuestLog_Update(addonName)
     end
 
     for i=1, numQuestsDisplayed, 1 do
-        local questIndex = i + FauxScrollFrame_GetOffset(_G[addonName.."ListScrollFrame"])
-        local questLogTitleText, level, questTag, isHeader, isCollapsed, isComplete, frequency, questID, startEvent, displayQuestID, isOnMap, hasLocalPOI, isTask, isBounty, isStory, isHidden, isScaling = GetQuestLogTitle(questIndex);
+        questLogTitle = buttons[i];
+        questIndex = i + scrollOffset;
+        questLogTitle:SetID(questIndex);
+        questTitleTag = questLogTitle.tag;
+        questNumGroupMates = questLogTitle.groupMates;
+        questCheck = questLogTitle.check;
+        questNormalText = questLogTitle.normalText
 
-        if isHeader then
-            -- TODO: work out a way of summarising without it breaking on collpase
-            -- if headerXP[questLogTitleText] > 0 then
-            --     local questHeaderText = _G[addonName.."ListScrollFrameButton"..i.."NormalText"]
-            --     questHeaderText:SetText(string.format("%s (%dxp)", questLogTitleText, headerXP[questLogTitleText]))
-            -- end
-        elseif QXPdb[questID] then
-            local questTitleTag = _G[addonName.."ListScrollFrameButton"..i.."Tag"]
-            local questTitleTagText = questTitleTag:GetText() or ""
-            local questNormalText = _G[addonName.."ListScrollFrameButton"..i.."NormalText"]
-            questTitleTag:SetText(string.format("(%d%s)%s", QXPdb[questID].XP, xpLeveLTag, questTitleTagText))
+        -- Need to get the quest info here, for the buttons
+        if ( questIndex <= numEntries ) then
+            questLogTitleText, level, questTag, isHeader, isCollapsed, isComplete, frequency, questID, startEvent, displayQuestID, isOnMap, hasLocalPOI, isTask, isBounty, isStory, isHidden, isScaling = GetQuestLogTitle(questIndex);
 
-
+            -- Set the quest tag
             if ( isComplete and isComplete < 0 ) then
                 questTag = FAILED;
             elseif ( isComplete and isComplete > 0 ) then
                 questTag = COMPLETE;
+            elseif ( frequency == LE_QUEST_FREQUENCY_DAILY ) then
+                if ( questTag ) then
+                    questTag = format(DAILY_QUEST_TAG_TEMPLATE, questTag);
+                else
+                    questTag = DAILY;
+                end
             end
-            -- Blizz code for calculating widths
+
             if ( questTag ) then
-                QuestLogDummyText:SetText("  "..questLogTitleText);
+                if QXPdb[questID] then
+                    questTitleTag:SetText(string.format("(%d%s)(%s)", QXPdb[questID].XP, xpLeveLTag, questTag));
+                else
+                    questTitleTag:SetText("("..questTag..")");
+                end
                 -- Shrink text to accomdate quest tags without wrapping
                 tempWidth = 275 - 15 - questTitleTag:GetWidth();
 
@@ -116,12 +119,30 @@ function QXP:QuestLog_Update(addonName)
                 else
                     textWidth = QuestLogDummyText:GetWidth();
                 end
-
                 questNormalText:SetWidth(tempWidth);
+                -- If there's quest tag position check accordingly
+                questTitleTag:Show();
+                --questCheck:Hide();
             else
-                if ( questNormalText:GetWidth() > 275 ) then
-                    questNormalText:SetWidth(260);
+                if QXPdb[questID] then
+                    questTitleTag:SetText(string.format("(%d%s)", QXPdb[questID].XP, xpLeveLTag));
+
+                    -- Shrink text to accomdate quest tags without wrapping
+                    tempWidth = 275 - 15 - questTitleTag:GetWidth();
+
+                    if ( QuestLogDummyText:GetWidth() > tempWidth ) then
+                        textWidth = tempWidth;
+                    else
+                        textWidth = QuestLogDummyText:GetWidth();
+                    end
+                    questNormalText:SetWidth(tempWidth);
+                    -- If there's quest tag position check accordingly
+                    questTitleTag:Show();
+                    --questCheck:Hide();
+                else
+                    questTitleTag:SetText("");
                 end
+
             end
         end
     end
