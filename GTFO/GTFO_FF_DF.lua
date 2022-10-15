4da0b740afe21c8e8f8e3fb35aeb44c6c5761a8f
zhaochengw <zhaochengw@qq.com>
2022-10-16 00:52:07 +0800

    Add GTFO

---
+++ GTFO/GTFO_FF_DF.lua
@@ -0,0 +1,14 @@
+--------------------------------------------------------------------------
+-- GTFO_FF_DF.lua 
+--------------------------------------------------------------------------
+--[[
+GTFO Friendly Fire List - Dragonflight
+]]--
+
+if (not (GTFO.ClassicMode or GTFO.BurningCrusadeMode or GTFO.WrathMode)) then
+
+-- ****************
+-- * Dragon Isles *
+-- ****************
+
+end
