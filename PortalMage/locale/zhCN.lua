52cc254056889668c8834ede45f6b09c9998e4eb
抓泥鳅的小帅 <zhaochengw@qq.com>
2024-06-14 15:16:20 +0800

    PortalMage添加中文翻译

---
+++ PortalMage/locale/zhCN.lua
@@ -0,0 +1,66 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("PortalMage", "zhCN")
+if not L then return end
+-- zhCN --
+-- options --
+L["Portal Mage"] = "法师传送门整合"
+L["Set Movable"] = "解锁"
+L["makes bar movable"] = "使本插件可移动"
+L["Layout"] = "样式"
+L["select layout type"] = "选择样式"
+L["Vertical"] = "横向"
+L["Horizontal"] = "竖排"
+L["Scale"] = "缩放"
+L["you scale whole bar up or down"] = "放大或缩小"
+L["Rune of Portals"] = "传送门材料"
+L["Rune of Teleportaion"] = "传送材料"
+L["Enable"] = "启用"
+L["enable Rune of Portals display near the bar"] = "启用传送门材料显示"
+L["enable Rune of Teleportaion display near the bar"] = "启用传送材料显示"
+L["Position"] = "位置"
+L["Top Left"] = "左上"
+L["Bottom Left"] = "左下"
+L["Top Right"] = "右上"
+L["Bottom Right"] = "右下"
+L["Invert mouse buttons"] = "切换鼠标动作"
+L["when enabled left mouse button will cast teleport spell while right mouse button will cast portal spell"] = "启用后鼠标左键施放传送，右键施放传送门"
+L["Opacity"] = "透明度"
+L["change bar opacity"] = "调整透明度"
+L["Show only on mouse over"] = "鼠标显示"
+L["when enabled bar will only be visible when you mouse over it"] = "启用后仅鼠标移动到传送条时方可显示"
+L["Portals"] = "传送门列表"
+
+-- Portals - Alliance -
+L["Stormwind"] = "暴风城"
+L["toggles portal and teleport to Stormwind"] = "开启到暴风城的传送/传送门显示"
+L["Ironforge"] = "铁炉堡"
+L["toggles portal and teleport to Ironforge"] = "开启到铁炉堡的传送/传送门显示"
+L["Darnassus"] = "达纳苏斯"
+L["toggles portal and teleport to Darnassus"] = "开启到达纳苏斯的传送/传送门显示"
+L["Exodar"] = "埃索达"
+L["toggles portal and teleport to Exodar"] = "开启到埃索达的传送/传送门显示"
+L["Theramore"] = "塞拉摩"
+L["toggles portal and teleport to Theramore"] = "开启到塞拉摩的传送/传送门显示"
+
+
+-- Portals - Horde -
+L["Orgrimmar"] = "奥格瑞玛"
+L["toggles portal and teleport to Orgrimmar"] = "开启到奥格瑞玛的传送/传送门显示"
+L["Undercity"] = "幽暗城"
+L["toggles portal and teleport to Undercity"] = "开启到幽暗城的传送/传送门显示"
+L["Thunder Bluff"] = "雷霆崖"
+L["toggles portal and teleport to Thunder Bluff"] = "开启到雷霆崖的传送/传送门显示"
+L["Silvermoon"] = "银月城"
+L["toggles portal and teleport to Silvermoon"] = "开启到银月城的传送/传送门显示"
+L["Stonard"] = "斯通纳德"
+L["toggles portal and teleport to Stonard"] = "开启到斯通纳德的传送/传送门显示"
+
+-- Portals - neutral city -
+
+L["Shattrath"] = "沙塔斯"
+L["toggles portal and teleport to Shattrath"] = "开启到沙塔斯的传送/传送门显示"
+L["Dalaran"] = "达拉然"
+L["toggles portal and teleport to Dalaran"] = "开启到达拉然的传送/传送门显示"
+
+-- toltip --
+L["Left Click:"] = "左键"
+L["Right Click:"] = "右键"
