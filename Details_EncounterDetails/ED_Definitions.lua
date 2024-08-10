4b360f65b18137e4aad0a95bd22d90f8f9447052
抓泥鳅的小帅 <zhaochengw@qq.com>
2024-08-10 23:26:49 +0800

    reduce Details

--- Details_EncounterDetails/ED_Definitions.lua
+++
@@ -1,8 +0,0 @@
-
----@class ed_barline : button
----@field statusBar statusbar
----@field lineText1 fontstring
----@field lineText4 fontstring
----@field statusBarTexture texture
----@field Icon texture
-
