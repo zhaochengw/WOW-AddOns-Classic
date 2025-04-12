bb69c9bf07a5db61cc35d8780715d535d40d54d1
抓泥鳅的小帅 <zhaochengw@qq.com>
2025-04-12 19:16:53 +0800

    remove AFKS

--- AFKS/Locales/zhCN.lua
+++
@@ -1,7 +0,0 @@
-if GetLocale() ~= "zhCN" then return end
-
--- Variables
-AFKS_CHATBAR_TEXT = "|cffff7fff悄悄话|r(%s) |cff00fffe战网|r(%s) |cffffc0c0频道|r(%s)"
-AFKS_CHATBAR_GUILD = "|cff40ff40公会|r(%s)"
-AFKS_NOGUILD = "没有公会"
-AFKS_DATEFORMAT = "%d年 %d月 %d日\n%s"
--- AFKS/Locales/zhTW.lua
+++
@@ -1,7 +0,0 @@
-if GetLocale() ~= "zhTW" then return end
-
--- Variables
-AFKS_CHATBAR_TEXT = "|cffff7fff悄悄話|r(%s) |cff00fffe戰網|r(%s) |cffffc0c0頻道|r(%s)"
-AFKS_CHATBAR_GUILD = "|cff40ff40公會|r(%s)"
-AFKS_NOGUILD = "沒有公會"
-AFKS_DATEFORMAT = "%d年 %d月 %d日\n%s"
